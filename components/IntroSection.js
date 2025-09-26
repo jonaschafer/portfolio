fc11233238956334081f7f283aec827305988fb5
Jon Schafer <jonaschafer@gmail.com>
2025-09-26 13:49:14 -0700

    Update IntroSection.js
    simpler syntax to get mondwest working

--- components/IntroSection.js
+++ components/IntroSection.js
@@ -3,7 +3,7 @@ export default function IntroSection() {
   return (
     <section className="bg-[#435938] min-h-screen flex items-center justify-center px-4">
       <div className="max-w-4xl mx-auto">
-        <p className="font-['PP_Mondwest:Regular',_sans-serif] text-[clamp(24px,_4vw,_31px)] text-neutral-50 leading-[1.2] tracking-[0.31px]">
+        <p className="font-['Mondwest',_sans-serif] text-[clamp(24px,_4vw,_31px)] text-neutral-50 leading-[1.2] tracking-[0.31px]">
           <span className="inline-block mr-2">👋</span>
           <span>Howdy! I'm Jon, Creative Director at ClassDojo based in rainy </span>
           <span className="inline-block mx-1">🌧️</span>
