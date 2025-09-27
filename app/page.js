e5b5f883cb0779cfb9ae487da21aed8bde00d2e0
Jon Schafer <jonaschafer@gmail.com>
2025-09-27 08:45:02 -0700

    add footer to page.js

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import IntroSection from '../components/IntroSection'
 import Navigation from '../components/Navigation'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <main>
       <Navigation />
       <IntroSection />
+      <Footer />
     </main>
   )
 }
