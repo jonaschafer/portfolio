c490dcd7097626b9df931c82745835b3e60c192b
Jon Schafer <jonaschafer@gmail.com>
2025-09-27 10:11:24 -0700

    moved up to top of page

--- components/IntroSection.js
+++ components/IntroSection.js
@@ -1,7 +1,7 @@
 export default function IntroSection() {
   return (
     <div className="bg-[#435938] w-full">
-      <section className="min-w-[375px] max-w-[1440px] mx-auto min-h-screen flex items-center">
+      <section className="min-w-[375px] max-w-[1440px] mx-auto min-h-screen flex items-start pt-[140px]">
         <div className="w-full px-5 md:px-[60px] lg:px-[60px]">
           <p className="font-['Mondwest',_sans-serif] text-[20px] md:text-[31px] lg:text-[31px] text-[#FAFAFA] leading-[1.2] tracking-[0.31px] max-w-[335px] md:max-w-[648px] lg:max-w-[747px] whitespace-pre-wrap">
             <span>👋 Howdy! I'm Jon, Creative Director at ClassDojo based in rainy  🌧️ Portland. Prev </span>
