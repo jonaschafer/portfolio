077bc4cf093c0ddba54dd879df410abd858097db
Jon Schafer <jonaschafer@gmail.com>
2025-09-27 08:34:37 -0700

    add nav

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import IntroSection from '../components/IntroSection'
+import Navigation from '../components/Navigation'
 
 export default function Home() {
   return (
     <main>
+      <Navigation />
       <IntroSection />
     </main>
   )
