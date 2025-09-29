0bcbb8ff85b070935250f18c5b82481b229bf556
Jon <jonaschafer@gmail.com>
2025-09-29 06:24:25 -0700

    added ascii art to homepage

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script id="made-with-claude" type="text/plain">{`
+ __  __           _                 _ _   _     
+|  \/  | __ _  __| | ___  __      _(_) |_| |__  
+| |\/| |/ _` |/ _` |/ _ \ \ \ /\ / / | __| '_ \ 
+| |  | | (_| | (_| |  __/  \ V  V /| | |_| | | |
+|_|__|_|\__,_|\__,_|\___|_  \_/\_/ |_|\__|_| |_|
+ / ___| | __ _ _   _  __| | ___                 
+| |   | |/ _` | | | |/ _` |/ _ \                
+| |___| | (_| | |_| | (_| |  __/                
+ \____|_|\__,_|\__,_|\__,_|\___|                
+`}</script>
+      </head>
       <body>{children}</body>
     </html>
   )
